816d482a9642ad90f3b999c2b5f4b552b3c8e77f
xt <32516078+xtzero@users.noreply.github.com>
2019-07-23 23:50:24 +0800

    add web index

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,29 +1,75 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container class="container">
+      <el-header style="height: 0px;"></el-header>
+      <el-main class="body">
+        <router-view/>
+      </el-main>
+      <el-footer class="footer">
+        <span>
+          <router-link to="/" class="el-icon-chat-line-round"> 表态</router-link>
+        </span>
+        <el-divider direction="vertical"></el-divider>
+        <span>
+          <router-link to="/smoking" class="el-icon-smoking"> 吸烟区</router-link>
+        </span>
+        <el-divider direction="vertical"></el-divider>
+        <span>
+          <router-link to="/me" class="el-icon-mobile-phone"> 自己</router-link>
+        </span>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
-<style lang="less">
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 44px;
+  border-top: solid 1px white;
+  background-color: black;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.footer a{
+  color: white;
+  text-decoration: none;
+  width: 100%;
+}
+
+.footer>span{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 33%;
+  text-align: center;
+}
+.body{
+  background-color: black;
+  height: calc(100vh - 43px);
+}
+body{
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,7 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="搭好了，晚上弄你"/>
+    <el-card class="box-card first-card">
+      <el-col :span="20">
+        <div class="notice-title">话题</div>
+        <div class="notice-content">本期主题：你为什么睡不着</div>
+      </el-col>
+      <el-col :span="4" class="first-card-col-right">
+        <el-button icon="el-icon-edit" type="primary" circle></el-button>
+      </el-col>
+    </el-card>
+    <el-card class="box-card">
+      <div>我觉得每天睡不着应该都赖杨艺，毕竟每天都是她絮絮叨叨不睡觉。</div>
+      <p class="time">
+        二狗子3天前发送
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-chat-dot-round">3</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-sunny">4</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-heavy-rain">5</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-view">135</span>
+      </p>
+    </el-card>
+    <el-card class="box-card">
+      <div>我觉得每天睡不着应该都赖杨艺，毕竟每天都是她絮絮叨叨不睡觉。</div>
+      <p class="time">
+        二狗子3天前发送
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-chat-dot-round">3</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-sunny">4</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-heavy-rain">5</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-view">135</span>
+      </p>
+    </el-card>
+    <el-card class="box-card">
+      <div>我觉得每天睡不着应该都赖杨艺，毕竟每天都是她絮絮叨叨不睡觉。</div>
+      <p class="time">
+        二狗子3天前发送
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-chat-dot-round">3</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-sunny">4</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-heavy-rain">5</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-view">135</span>
+      </p>
+    </el-card>
+    <el-card class="box-card">
+      <div>我觉得每天睡不着应该都赖杨艺，毕竟每天都是她絮絮叨叨不睡觉。</div>
+      <p class="time">
+        二狗子3天前发送
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-chat-dot-round">3</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-sunny">4</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-heavy-rain">5</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-view">135</span>
+      </p>
+    </el-card>
+    <el-card class="box-card">
+      <div>我觉得每天睡不着应该都赖杨艺，毕竟每天都是她絮絮叨叨不睡觉。</div>
+      <p class="time">
+        二狗子3天前发送
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-chat-dot-round">3</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-sunny">4</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-heavy-rain">5</span>
+        <el-divider direction="vertical"></el-divider>
+        <span class="el-icon-view">135</span>
+      </p>
+    </el-card>
   </div>
 </template>
 
@@ -16,3 +93,37 @@ export default {
   }
 }
 </script>
+<style>
+  .box-card{
+    font-size: 20px;
+    text-align: left;
+    border-radius: 2px;
+    margin: 15px 0;
+  }
+
+  .time{
+    color: #999;
+    margin: 10px 0 -5px 0;
+    font-size: 13px;
+  }
+
+  .notice-title{
+    font-size: 20px;
+  }
+
+  .notice-content{
+    font-size: 13px;
+  }
+
+  .first-card{
+    padding: 0 0 20px 0;
+  }
+
+  .first-card-col-right{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-top: 5px;
+  }
+</style>
