927cb8a2e8c6ab306c2174b8e90863241e2dc244
xt <32516078+xtzero@users.noreply.github.com>
2019-07-24 00:19:25 +0800

    web ver 1

--- web/src/views/Me.vue
+++ web/src/views/Me.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <el-card class="box-card">
+      <div>二狗子</div>
+      <p class="time">
+        3 条内容
+      </p>
+      <el-divider></el-divider>
+      <p class="time card-bottom">
+        <i class="el-icon-cold-drink"> 重生</i>
+        <span style="margin-left: 20px;"></span>
+        <i class="el-icon-moon-night"> 转世</i>
+      </p>
+    </el-card>
+    <el-card class="box-card">
+      <div class="text item">
+        <div>我觉得每天睡不着应该都赖杨艺，毕竟每天都是她絮絮叨叨不睡觉。</div>
+        <p class="time">
+          3天前发送
+          <el-divider direction="vertical"></el-divider>
+          <span class="el-icon-chat-dot-round">3</span>
+          <el-divider direction="vertical"></el-divider>
+          <span class="el-icon-sunny">4</span>
+          <el-divider direction="vertical"></el-divider>
+          <span class="el-icon-heavy-rain">5</span>
+          <el-divider direction="vertical"></el-divider>
+          <span class="el-icon-view">135</span>
+        </p>
+        <el-divider></el-divider>
+        <p class="time card-bottom">
+          <i class="el-icon-delete"> 删除</i>
+        </p>
+      </div>
+    </el-card>
   </div>
 </template>
+<style>
+  .card-bottom{
+    margin-top: -10px;
+  }
+</style>
