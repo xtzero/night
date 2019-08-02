3393162408ad5249f5a8526c03b8789cfda8e9aa
xt <32516078+xtzero@users.noreply.github.com>
2019-08-03 00:13:09 +0800

    先上一版吧

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home" v-loading="screenLoading">
+<!--    话题卡片-->
     <el-card class="box-card first-card">
       <el-col :span="20">
         <div class="notice-title">话题</div>
@@ -9,6 +10,7 @@
         <el-button icon="el-icon-edit" type="primary" circle @click="send.display=true"></el-button>
       </el-col>
     </el-card>
+<!--    帖子卡片 循环-->
     <el-card
       class="box-card"
       v-for="v in list"
@@ -17,7 +19,7 @@
         {{ v.content }}
       </div>
       <p class="time" @click="showDetail(v)">
-        {{ v.name }} 于 {{ formatTime(v.create_at)}} 发送
+        {{ zipName(v.name) }} 于 {{ formatTime(v.create_at)}} 发送
         <br/>
         <span class="el-icon-chat-dot-round">{{ v.c_count ? v.c_count : 0 }}</span>
         <el-divider direction="vertical"></el-divider>
@@ -26,17 +28,19 @@
         <span class="el-icon-heavy-rain">{{ v.shit ? v.shit : 0 }}</span>
       </p>
     </el-card>
+<!--    下一页卡片-->
     <el-card class="box-card" style="text-align: center; font-size: 13px;padding: 0;" v-show="!nomore">
 			<span @click="nextPage">
 				下一页
 			</span>
     </el-card>
+<!--    没有下一页了提示重新加载-->
     <el-card class="box-card" style="text-align: center; font-size: 13px;padding: 0;" v-show="nomore">
 			<span @click="reload">
 				没有更多了，点击重新加载
 			</span>
     </el-card>
-
+<!--新发帖 弹层-->
     <el-dialog
       title="想要表态"
       :visible.sync="send.display"
@@ -53,16 +57,16 @@
             :autosize="{ minRows: 3, maxRows: 10}"
           >
           </el-input>
-          <span style="color: red;" v-if="sendTotalCount>30">
-            剩余字数 {{sendTotalCount}} / 30
+          <span style="color: red;" v-if="this.send.data.content.length>30">
+            剩余字数 {{this.send.data.content.length}} / 30
           </span>
           <span v-else>
-            剩余字数 {{sendTotalCount}} / 30
+            剩余字数 {{this.send.data.content.length}} / 30
           </span>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="sendMoment" :disabled="sendTotalCount>30">表态！</el-button>
+        <el-button type="primary" @click="sendMoment" :disabled="this.send.data.content.length > 30">表态！</el-button>
       </div>
     </el-dialog>
 
@@ -72,19 +76,23 @@
       :fullscreen="true"
       v-loading="detail.loading"
     >
-      <h3><i class="el-icon-sugar" style="font-size: 20px;"></i> {{detail.data.name}} 说：</h3>
+      <h3><i class="el-icon-sugar" style="font-size: 20px;"></i> {{zipName(detail.data.name)}} 说：</h3>
       <span class="detail-content">{{detail.data.content}}</span>
       <p class="time" style="margin-bottom: 30px;">{{detail.data.create_at}}</p>
-<!--      <el-row style="display: flex;justify-content: center;align-items: center">-->
-<!--        <el-button type="primary" icon="el-icon-sunny" circle></el-button>-->
-<!--        <el-button type="primary" icon="el-icon-heavy-rain" circle></el-button>-->
-<!--      </el-row>-->
       <div class="block">
+        <el-row style="display: flex;flex-direction: row;justify-content: space-between;width: 100%;">
+          <el-col :span="6" style="text-align: left;">
+            <i class="el-icon-sunny"></i> {{detail.data.star}}
+          </el-col>
+          <el-col :span="12" style="text-align: center;font-size: 10px;">{{sosTip}}</el-col>
+          <el-col :span="6" style="text-align: right;">
+            {{detail.data.shit}} <i class="el-icon-heavy-rain"></i>
+          </el-col>
+        </el-row>
         <el-slider
-          v-model="value"
-          range
+          v-model="detail.sos"
           :show-tooltip="false"
-          
+          @change="onSosChange"
         >
         </el-slider>
       </div>
@@ -92,17 +100,48 @@
       <el-divider class="others-say-divider" v-else>其他人啥都没说</el-divider>
       <el-card class="detail-box-card">
         <div class="text item" style="text-align: center;">
-          <span class="el-icon-thumb" v-if="hasComments"> 加入 「其他人」</span>
-          <span class="el-icon-thumb" v-else> 我先说点啥</span>
+          <span class="el-icon-thumb" v-if="hasComments" @click="showCommentDialog"> 加入 「其他人」</span>
+          <span class="el-icon-thumb" v-else @click="showCommentDialog"> 我先说点啥</span>
         </div>
       </el-card>
       <el-card class="detail-box-card" v-for="v in detail.data.comments" v-show="hasComments">
-        <span>{{v.name}} :</span>
+        <span>{{zipName(v.name)}} :</span>
         <div class="text item">
           {{v.content}}
         </div>
       </el-card>
       <el-divider class="others-say-divider" v-if="hasComments">其他人说完了</el-divider>
+
+    <!--详情里的评论弹层-->
+      <el-dialog
+              title="想要表态"
+              :visible.sync="detail.comment.display"
+              width="100%"
+              v-loading="detail.comment.loading"
+              append-to-body
+      >
+        <el-form>
+          <el-form-item label-width="0">
+            <el-input
+                    :rows="10"
+                    type="textarea"
+                    placeholder="请输入内容"
+                    v-model="detail.comment.data.content"
+                    :autosize="{ minRows: 3, maxRows: 10}"
+            >
+            </el-input>
+            <span style="color: red;" v-if="detail.comment.data.content.length > 30">
+            剩余字数 {{detail.comment.data.content.length}} / 30
+          </span>
+            <span v-else>
+            剩余字数 {{detail.comment.data.content.length}} / 30
+          </span>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="primary" @click="sendComment" :disabled="detail.comment.data.content.length > 30">表态！</el-button>
+        </div>
+      </el-dialog>
     </el-dialog>
   </div>
 </template>
@@ -127,8 +166,18 @@
         detail: {
           loading: false,
           display: false,
+          sos: 0,
           data: {
-            comments: []
+            comments: [],
+            shit: 0,
+            star: 0
+          },
+          comment: {
+            display: false,
+            data: {
+              content: ''
+            },
+            loading: false
           }
         }
       }
@@ -206,6 +255,7 @@
           if (res.data.code === 200) {
             this.detail.display = true
             this.detail.data = res.data.data
+            this.detail.sos = this.sosOriValue()
             console.log(this.detail.data)
           } else {
             this.$message({
@@ -214,14 +264,117 @@
             })
           }
         })
+      },
+      onSosChange(v) {
+        if (v === 0) {
+          this.shit()
+        } else if (v === 100) {
+          this.star()
+        } else {
+          this.detail.sos = this.sosOriValue()
+        }
+      },
+      sosOriValue() {
+        const shit = this.detail.data.shit
+        const star = this.detail.data.star
+        const v = (star - shit) + 50
+        return v
+      },
+      star() {
+        this.detail.loading = true
+        this.$utils.ajax('starOrShit', {
+          mid: this.detail.data.id,
+          sos: 'star'
+        }).then((res) => {
+          this.detail.loading = false
+          if (res.data.code === 200) {
+            this.showDetail({id: this.detail.data.id})
+            this.$message({
+              type: 'success',
+              message: '点赞成功！',
+              showClose: true,
+            })
+          } else {
+            this.$message({
+              type: 'error',
+              message: res.data.msg,
+              showClose: true
+            })
+          }
+        }).catch((e) => {
+          this.detail.data.sos = this.sosOriValue()
+        })
+      },
+      shit() {
+        this.detail.loading = true
+        this.$utils.ajax('starOrShit', {
+          mid: this.detail.data.id,
+          sos: 'shit'
+        }).then((res) => {
+          this.detail.loading = false
+          if (res.data.code === 200) {
+            this.showDetail({id: this.detail.data.id})
+            this.$message({
+              type: 'success',
+              message: '点踩成功！',
+              showClose: true
+            })
+          } else {
+            this.$message({
+              type: 'error',
+              message: res.data.msg,
+              showClose: true
+            })
+          }
+        }).catch((e) => {
+          this.detail.data.sos = this.sosOriValue()
+        })
+      },
+      showCommentDialog() {
+        this.detail.comment.display = true
+      },
+      sendComment() {
+        this.detail.comment.loading = true
+        this.$utils.ajax('sendComment', {
+          mid: this.detail.data.id,
+          userid: this.$store.state.user.id,
+          content: this.detail.comment.data.content
+        }).then((res) => {
+          this.detail.comment.loading = false
+          if (res.data.code === 200) {
+            this.detail.comment.display = false
+            this.detail.comment.data.content = ''
+            this.showDetail({
+              id: this.detail.data.id
+            })
+          } else {
+            this.$message({
+              type: 'error',
+              message: res.data.msg
+            })
+          }
+        })
+      },
+      zipName(name) {
+        return name && name.length > 8 ? name.substring(0,8) + '...' : name
       }
     },
     computed: {
-      sendTotalCount() {
-        return this.send.data.content.length
-      },
       hasComments() {
         return this.detail.data.comments.length > 0 ? true : false
+      },
+      sosTip() {
+        if (this.detail.sos == this.sosOriValue()) {
+          return '拖动滑块来表态'
+        } else if (this.detail.sos < this.sosOriValue() && this.detail.sos > 0) {
+          return '拖到最左表示反对'
+        } else if (this.detail.sos > this.sosOriValue() && this.detail.sos < 100) {
+          return '拖到最右表示赞同'
+        } else if (this.detail.sos == 100) {
+          return '松手表示赞同'
+        } else if (this.detail.sos == 0) {
+          return '松手表示反对'
+        }
       }
     }
   }
@@ -271,5 +424,13 @@
 
   .others-say-divider{
     margin: 50px 0;
+  }
+
+  .el-slider__bar{
+    background-color: greenyellow !important;
+  }
+
+  .el-slider__runway{
+    background-color: red !important;
   }
 </style>
--- web/src/views/Me.vue
+++ web/src/views/Me.vue
@@ -1,138 +1,195 @@
 <template>
-  <div class="container" v-loading="screenLoading">
-    <el-card class="box-card">
-      <div style="font-weight: 900;font-size: 25px;">{{me.name.length > 8 ? me.name.substring(0,8) + '...' : me.name}}</div>
-      <p class="time">
-        {{me.count}} 条内容
-      </p>
-      <el-divider></el-divider>
-      <p class="time card-bottom">
-        <i class="el-icon-cold-drink"> 重生</i>
-        <span style="margin-left: 20px;"></span>
-        <i class="el-icon-moon-night"> 转世</i>
-      </p>
-    </el-card>
-    <el-card class="box-card" v-for="v in list">
-      <div>
-        {{ v.content }}
-      </div>
-      <p class="time">
-        {{ v.name }} 于 {{ formatTime(v.create_at)}} 发送
-        <br/>
-        <span class="el-icon-chat-dot-round">{{ v.c_count ? v.c_count : 0 }}</span>
-        <el-divider direction="vertical"></el-divider>
-        <span class="el-icon-sunny">{{ v.star ? v.star : 0 }}</span>
-        <el-divider direction="vertical"></el-divider>
-        <span class="el-icon-heavy-rain">{{ v.shit ? v.shit : 0 }}</span>
-      </p>
-      <el-divider></el-divider>
-      <p class="time card-bottom">
-        <i class="el-icon-delete"> 删除</i>
-      </p>
-    </el-card>
-    <el-card class="box-card" style="text-align: center; font-size: 13px;padding: 0;" v-show="!nomore">
+	<div class="container" v-loading="screenLoading">
+		<el-card class="box-card">
+			<div style="font-weight: 900;font-size: 25px;">{{me.name.length > 8 ? me.name.substring(0,8) + '...' :
+				me.name}}
+			</div>
+			<p class="time">
+				{{me.count}} 条内容
+			</p>
+			<el-divider></el-divider>
+			<p class="time card-bottom">
+				<i class="el-icon-cold-drink" @click="showReturnBeforeDialog"> 重生</i>
+				<span style="margin-left: 20px;"></span>
+				<i class="el-icon-moon-night" @click="showRebornDialog"> 转世</i>
+			</p>
+		</el-card>
+		<el-card class="box-card" v-for="v in list">
+			<div>
+				{{ v.content }}
+			</div>
+			<p class="time">
+				{{ v.name }} 于 {{ formatTime(v.create_at)}} 发送
+				<br/>
+				<span class="el-icon-chat-dot-round">{{ v.c_count ? v.c_count : 0 }}</span>
+				<el-divider direction="vertical"></el-divider>
+				<span class="el-icon-sunny">{{ v.star ? v.star : 0 }}</span>
+				<el-divider direction="vertical"></el-divider>
+				<span class="el-icon-heavy-rain">{{ v.shit ? v.shit : 0 }}</span>
+			</p>
+			<el-divider></el-divider>
+			<p class="time card-bottom">
+				<i class="el-icon-delete" @click="commitDelete(v)"> 删除</i>
+			</p>
+		</el-card>
+		<el-card class="box-card" style="text-align: center; font-size: 13px;padding: 0;" v-show="!nomore">
 			<span @click="nextPage">
 				下一页
 			</span>
-    </el-card>
-    <el-card class="box-card" style="text-align: center; font-size: 13px;padding: 0;" v-show="nomore">
+		</el-card>
+		<el-card class="box-card" style="text-align: center; font-size: 13px;padding: 0;" v-show="nomore">
 			<span @click="reload">
 				没有更多了，点击重新加载
 			</span>
-    </el-card>
-  </div>
+		</el-card>
+		<el-dialog
+				title="转世"
+				:visible.sync="reborn.display"
+				width="80%"
+		>
+			<span>
+				<p>转世之后会重置当前登录状态，并且你将永远失去当前身份。</p>
+				<p>你将会失去当前的全部数据，并使重生口令失效。</p>
+				<p>你可以就此重新开始。</p>
+				<p>如果决定了，请点击确定。</p>
+			</span>
+			<span slot="footer" class="dialog-footer">
+			    <el-button @click="reborn.display = false">取 消</el-button>
+			    <el-button type="primary" @click="commitReborn()">确 定</el-button>
+			</span>
+		</el-dialog>
+		<el-dialog
+				title="重生"
+				:visible.sync="returnBefore.display"
+				width="80%"
+		>
+			<span>
+				<p>输入登录时预设的重生口令，你可以将之前账号的数据完全同步过来。</p>
+				<p>将会参与同步的数据有：帖子、评论</p>
+				<p>如果决定好了，请告诉我你的重生口令...</p>
+				<el-input
+						v-model="returnBefore.password"
+						placeholder="安巴尼妈咪哄"
+				/>
+			</span>
+			<span slot="footer" class="dialog-footer">
+			    <el-button @click="returnBefore.display = false">取 消</el-button>
+			    <el-button type="primary" @click="commitReturnBefore()">确 定</el-button>
+			</span>
+		</el-dialog>
+	</div>
 </template>
 <script>
-  export default {
-    name: 'me',
-    components: {},
-    data() {
-      return {
-        screenLoading: false,
-        list: [],
-        page: 1,
-        nomore: false,
-        me: {
-          count: 0,
-          name: ''
-        }
-      }
-    },
-    mounted() {
-      this.$checkUser()
-      this.me.name = this.$store.state.user.name
-      this.getMomentCount()
-      this.getMomentList()
-    },
-    methods: {
-      getMomentCount() {
-        this.screenLoading = true
-        this.$utils.ajax("momentCount", {
-          userid: this.$store.state.user.id
-        }).then((res) => {
-          this.screenLoading = false
-          if (res.data.code === 200) {
-            this.me.count = res.data.data.count
-          } else {
-            this.$notify({
-              type: 'error',
-              message: res.data.msg
-            })
-          }
-        })
-      },
-      getMomentList(nextPage) {
-        let page;
-        if (nextPage) {
-          page = this.page + 1
-        } else {
-          page = this.page
-        }
-        this.screenLoading = true
-        this.$utils.ajax('listMoment', {
-          page: page,
-          userid: this.$store.state.user.id
-        }).then((res) => {
-          this.screenLoading = false
-          if (res.data.code == 200) {
-            const d = res.data.data
-            for (const i in d) {
-              this.list.push(d[i])
+    export default {
+        name: 'me',
+        components: {},
+        data() {
+            return {
+                screenLoading: false,
+                list: [],
+                page: 1,
+                nomore: false,
+                me: {
+                    count: 0,
+                    name: ''
+                },
+                reborn: {
+                    display: false
+                },
+                returnBefore: {
+                    display: false,
+	                password: ''
+                }
             }
-            if (nextPage) {
-              this.page++
-            }
-          } else if (res.data.msg == '没有更多数据了') {
-            this.nomore = true
-          } else {
-            this.$notify({
-              type: 'error',
-              message: res.data.msg
-            })
-          }
-        })
-      },
-      formatTime(time) {
-        return this.$utils.formatTime(time)
-      },
-      nextPage() {
-        this.getMomentList(true)
-      },
-      reload() {
-        this.list = []
-        this.page = 1
-        this.nomore = false
-        this.getMomentList()
-      }
+        },
+        mounted() {
+            this.$checkUser()
+            this.me.name = this.$store.state.user.name
+            this.getMomentCount()
+            this.getMomentList()
+        },
+        methods: {
+            getMomentCount() {
+                this.screenLoading = true
+                this.$utils.ajax("momentCount", {
+                    userid: this.$store.state.user.id
+                }).then((res) => {
+                    this.screenLoading = false
+                    if (res.data.code === 200) {
+                        this.me.count = res.data.data.count
+                    } else {
+                        this.$notify({
+                            type: 'error',
+                            message: res.data.msg
+                        })
+                    }
+                })
+            },
+            getMomentList(nextPage) {
+                let page;
+                if (nextPage) {
+                    page = this.page + 1
+                } else {
+                    page = this.page
+                }
+                this.screenLoading = true
+                this.$utils.ajax('listMoment', {
+                    page: page,
+                    userid: this.$store.state.user.id
+                }).then((res) => {
+                    this.screenLoading = false
+                    if (res.data.code == 200) {
+                        const d = res.data.data
+                        for (const i in d) {
+                            this.list.push(d[i])
+                        }
+                        if (nextPage) {
+                            this.page++
+                        }
+                    } else if (res.data.msg == '没有更多数据了') {
+                        this.nomore = true
+                    } else {
+                        this.$notify({
+                            type: 'error',
+                            message: res.data.msg
+                        })
+                    }
+                })
+            },
+            formatTime(time) {
+                return this.$utils.formatTime(time)
+            },
+            nextPage() {
+                this.getMomentList(true)
+            },
+            reload() {
+                this.list = []
+                this.page = 1
+                this.nomore = false
+                this.getMomentList()
+            },
+	        showRebornDialog() {
+                this.reborn.display = true
+	        },
+            showReturnBeforeDialog() {
+                this.returnBefore.display = true
+            },
+	        commitReborn() {
+                alert('转世！')
+	        },
+	        commitReturnBefore() {
+                alert('重生！')
+	        },
+	        commitDelete(v) {
+                alert('要删除啦')
+	        }
 
-    },
-    computed: {
-
+        },
+        computed: {}
     }
-  }
 </script>
 <style>
-  .card-bottom{
-    margin-top: -10px;
-  }
+	.card-bottom {
+		margin-top: -10px;
+	}
 </style>
