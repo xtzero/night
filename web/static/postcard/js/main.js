d9c6381dbfa09afe17255b148a403e5ce522533a
xt <t@xtzero.me>
2020-02-10 16:10:18 +0800

    能取件了

--- web/static/postcard/js/main.js
+++ web/static/postcard/js/main.js
@@ -3,11 +3,47 @@ $(function(){
     $('.message').click(function(){
         $('#verifycode').show()
     })
+    // 输入取件码点了取件
     $('.code-get').click(function(){
-        $('#verifycode').hide()
-        $('.message').hide()
-        $('.popBg').show()
-        $('#postcard').show()
+        $('.code-get').text('获取中')
+        var verifyCode = $('#code-input').val()
+        if (!verifyCode) {
+            a('请输入取件码')
+            $('.code-get').text('取件')
+            return
+        }
+
+        $.ajax({
+            url: 'http://nightapi.xtzero.me/index.php/readPostcard',
+            dataType: "json",
+            async: true,
+            data: {
+                verifycode: verifyCode
+            },
+            type: "GET",
+            beforeSend: function(){
+                console.log(1)
+            },
+            success: function(req){
+                if (req.code == 200) {
+                    console.log(req)
+                    $('#content').text(req.data[0].content)
+                    $('#from').text('from:' + req.data[0].from_name)
+                    $('#verifycode').hide()
+                    $('.message').hide()
+                    $('.popBg').show()
+                    $('#postcard').show()
+                } else {
+                    a(req.msg)
+                }
+            },
+            complete: function(){
+                $('.code-get').text('取件')
+            },
+            error: function(){
+                alert('参加人数太多啦，一会再来吧~')
+            }
+        })
     })
     $('.close').click(function(){
         $('.message').show()
@@ -15,3 +51,7 @@ $(function(){
         $('#postcard').hide()
     })
 })
+
+function a(message) {
+    alert(message)
+}
